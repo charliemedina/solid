58c4fb62a8d97e40e88c42d678cf3e316e001da9
charliemedina <charliemedina1989@gmail.com>
2020-06-10 19:41:21 +0200

    Dependency inversion principle after applying the changes.

--- SOLID/SOLID/Program.cs
+++ SOLID/SOLID/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SOLID
 {
@@ -8,13 +9,21 @@ namespace SOLID
         {
             #region Shapes
 
+            var shapes = new IGeometricShape[] {
+                new Rectangle { Width = 3, Height = 4 },
+                new Rectangle { Width = 4, Height = 5 },
+                new Rectangle { Width = 5, Height = 6 },
+                new EquilateralTriangle { SideLenght = 3 }
+            };
+
             var calculator = new ShapesCalculator();
-            calculator.Calculate();
+            calculator.Calculate(shapes);
             Console.WriteLine($"Total area: {calculator.TotalAreas} \n" +
                               $"Total perimeter: {calculator.TotalPerimeters}");
+
             Console.ReadKey();
 
-            #endregion
-        }
+        #endregion
+    }
     }
 }
--- SOLID/SOLID/ShapesCalculator.cs
+++ SOLID/SOLID/ShapesCalculator.cs
@@ -1,20 +1,15 @@
-﻿namespace SOLID
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace SOLID
 {
     public class ShapesCalculator
     {
         public double TotalAreas { get; private set; }
         public double TotalPerimeters { get; private set; }
 
-        public void Calculate()
+        public void Calculate(IEnumerable<IGeometricShape> shapes)
         {
-            var shapes = new IGeometricShape[]
-            {
-                new Rectangle { Width = 3, Height = 4 },
-                new Rectangle { Width = 4, Height = 5 },
-                new Rectangle { Width = 5, Height = 6 },
-                new EquilateralTriangle { SideLenght = 3 }
-            };
-
             var areaOperations = new AreaOperations();
             var perimeterOperations = new PerimeterOperations();
 
