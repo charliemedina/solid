6f13816acda8860069739db737ef2935e4a039f6
charliemedina <charliemedina1989@gmail.com>
2020-06-10 15:59:04 +0200

    Single Responsibility Principle before applying the changes.
    "A class should have only one reason to change"

---
+++ SOLID/SOLID/Rectangle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SOLID
+{
+    public class Rectangle
+    {
+        public double Sides { get; } = 4;
+        public double Height { get; set; }
+        public double Width { get; set; }
+
+        public static double SumAreas(IEnumerable<Rectangle> rectangles)
+        {
+            return rectangles.Sum(r => r.Height * r.Width);
+        }
+
+        public static double SumPerimeters(IEnumerable<Rectangle> rectangles)
+        {
+            return rectangles.Sum(r => 2* r.Height + 2 * r.Width);
+        }
+    }
+}
---
+++ SOLID/UnitTest/ShapesTest.cs
@@ -0,0 +1,40 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SOLID;
+using System.Collections.Generic;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class ShapesTest
+    {
+        [TestMethod]
+        public void Sum_Areas()
+        {
+            var rectangles = Rectangles;
+
+            double result = Rectangle.SumAreas(rectangles);
+
+            Assert.IsTrue(result == totalAreas);
+        }
+
+        [TestMethod]
+        public void Sum_Perimeters()
+        {
+            var rectangles = Rectangles;
+
+            double result = Rectangle.SumPerimeters(rectangles);
+
+            Assert.IsTrue(result == totalPerimeters);
+        }
+
+        private IEnumerable<Rectangle> Rectangles => new [] {
+            new Rectangle { Width = 3, Height = 4 },
+            new Rectangle { Width = 4, Height = 5 },
+            new Rectangle { Width = 5, Height = 6 } 
+        };
+
+        private readonly double totalAreas = 62;
+        private readonly double totalPerimeters = 54;
+    }
+
+}
